b02396b9eeae8f3c59bc58afaa58ed6cc4db0b6c
george <20103125@mail.wit.ie>
2023-02-19 12:07:15 +0000

    Removed test code from index.css, started work on home page, added test menu (no links)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const title = <h1>Beer Counter</h1>;
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+      <header>{title}</header>
+
     </div>
   );
 }
+
+const Menu = () => {
+  <nav>
+    <a>Home</a>
+    <a>Leaderboard</a>
+    <a>Account</a>
+  </nav>
+}
+
 
 export default App;
